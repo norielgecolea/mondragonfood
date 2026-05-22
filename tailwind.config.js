4be25df1ebc71bd7137c30aa8b2b4cce23920335
Noriel Gecolea <angelgecolea23@gmail.com>
2026-05-22 17:42:03 +0800

    Deploy Angular app

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "120rem",
+      },
+    },
+  },
+  plugins: [],
+}
+
